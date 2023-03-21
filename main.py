a746a56136f8b708bfff8b32da3061d7a1934017
Alex Zernov <93455802+Omletos@users.noreply.github.com>
2023-03-21 20:35:56 +0000

    Added text box for the shift amount.

--- main.py
+++ main.py
@@ -18,3 +18,9 @@ text_label = Label(root, text="Enter text to encrypt:")
 text_label.pack()
 text_box = Entry(root, width=40)
 text_box.pack()
+
+# Shift input box
+shift_label = Label(root, text="Enter shift amount (0-25):")
+shift_label.pack()
+shift_box = Entry(root, width=20)
+shift_box.pack()
